efcbf077914ac3c879689e46b0f400b66fef10d8
Evgeny <evgenytikhonov@oulook.com>
2020-06-29 21:36:24 +0300

    return JSON response per view

--- api/views.py
+++ api/views.py
@@ -1,24 +1,21 @@
 from datetime import datetime
 
+from django.http import JsonResponse
 from django.shortcuts import render
 
 # Create your views here.
 from rest_framework import status
-from rest_framework.generics import CreateAPIView
+from rest_framework.generics import GenericAPIView
 from rest_framework.response import Response
 
 from api.models import ZoneCamera, CountLogData
 from api.serializer import ZoneCameraSerializer
 
 
-class PeopleCountUpdate(CreateAPIView):
+class PeopleCountUpdate(GenericAPIView):
     serializer_class = ZoneCameraSerializer
 
     def post(self, request, *args, **kwargs):
-        # ip = request.data['ip_addr']
-        # image = request.data['image']
-        # count = request.data['count']
-        # beach_id = request.data['beach_id']
         cam_nam = request.data['cam_name']
         cam_object = ZoneCamera.objects.get(cam_name=cam_nam)
         _log_data = {
@@ -30,19 +27,19 @@ class PeopleCountUpdate(CreateAPIView):
         if serializer.is_valid():
             serializer.save()
         else:
-            return Response(data={'msg': 'Wrong Parameters'}, status=status.HTTP_400_BAD_REQUEST)
+            return JsonResponse(data={'msg': 'Wrong Parameters'}, status=status.HTTP_400_BAD_REQUEST)
 
         new_log = CountLogData.objects.create(**_log_data)
         new_log.save()
-        return Response(data={'date': new_log.date, 'count': new_log.count},
+        return JsonResponse(data={'date': new_log.date, 'count': new_log.count},
                         status=status.HTTP_200_OK)
 
     def get(self, request, *args, **kwargs):
         try:
-            cam_nam = request.get['cam_name']
+            cam_nam = request.GET['cam_name']
             cam_object = ZoneCamera.objects.get(cam_name=cam_nam)
             serializer = self.serializer_class(cam_object)
-            return Response(serializer.data, status=status.HTTP_200_OK)
+            return JsonResponse(serializer.data, status=status.HTTP_200_OK)
         except:
-            return Response({'Error': 'Wrong input'}, status=status.HTTP_400_BAD_REQUEST)
+            return JsonResponse({'Error': 'Wrong input'}, status=status.HTTP_400_BAD_REQUEST)
 
